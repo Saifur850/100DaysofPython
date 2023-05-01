57d1643724226f735a26ccbb67fb9148c61a4fa4
Saifur Rahman <100674849+Saifur850@users.noreply.github.com>
2023-05-01 10:16:04 +0600

    Delete Exercise2-4.py

--- Exercise2-4.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to tip calculator ")
-total_bill = input("What was the total bill? ")
-tip_perc = input("What percentage tip do you like to give? ")
-total_people = input("How many people tp split the bill? ")
-
-pay_amount = float(total_bill) * (1 + (int(tip_perc) / 100))
-indivisual_amount = float(pay_amount) / int(total_people)
-
-print(f"Each person should pay: {round(indivisual_amount,2)}")  
